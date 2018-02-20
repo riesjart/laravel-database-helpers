0ece01082668429bffe75a392cef2e734818f0c8
Richard van Baarsen <richardvanbaarsen@gmail.com>
2018-02-20 11:35:55 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,30 @@
+## Version
+
+Current release: **WIP**
+
+This repository uses [Semantic Versioning (SemVer) v2.0.0](http://semver.org/spec/v2.0.0.html).
+
+## Requirements
+
+- PHP >= 7.1
+- Laravel >= 5.6
+
+## Installation
+
+Pull this package in through Composer.
+
+```
+composer require riesjart/laravel-helpers "^0.1"
+```
+
+There is no service provider that needs to be registered in your Laravel application.
+
+## Features
+
+### Helper functions
+
+- mysql_version
+- normalize_connection
+- query_dump
+- query_listen
+- query_log
---
+++ src/helpers.php
@@ -0,0 +1,110 @@
+<?php
+
+use Illuminate\Container\Container;
+use Illuminate\Database\Connection;
+use Illuminate\Database\Events\QueryExecuted;
+use Symfony\Component\VarDumper\VarDumper;
+
+if ( ! function_exists('mysql_version')) {
+
+    /**
+     * @param Connection|string|null $connection
+     *
+     * @return string|null
+     */
+    function mysql_version($connection = null): ? string
+    {
+        $connection = normalize_connection($connection);
+
+        if ($connection->getDriverName() === 'mysql') {
+
+            $version = $connection->select("SHOW VARIABLES LIKE 'innodb_version'")[0]->Value;
+
+            return explode('-', $version)[0];
+        }
+
+        return null;
+    }
+}
+
+if ( ! function_exists('normalize_connection')) {
+
+    /**
+     * @param Connection|string|null $connection
+     *
+     * @return Connection
+     */
+    function normalize_connection($connection = null): Connection
+    {
+        if ( ! $connection instanceof Connection) {
+
+            $connection = Container::getInstance()
+                ->make('db')
+                ->connection($connection);
+        }
+
+        return $connection;
+    }
+}
+
+if ( ! function_exists('query_dump')) {
+
+    /**
+     * @param Connection|string|null $connection
+     *
+     * @return void
+     */
+    function query_dump($connection = null): void
+    {
+        query_listen(function ($query, $time): void {
+
+            VarDumper::dump(compact('query', 'time'));
+
+        }, $connection);
+    }
+}
+
+if ( ! function_exists('query_listen')) {
+
+    /**
+     * @param Closure $callback
+     * @param Connection|string|null $connection
+     *
+     * @return void
+     */
+    function query_listen(Closure $callback, $connection = null): void
+    {
+        $connection = normalize_connection($connection);
+
+        $connection->listen(function (QueryExecuted $event) use ($callback): void {
+
+            $sql = $event->sql;
+
+            foreach ($event->bindings as $binding) {
+
+                $sql = preg_replace('/\?/', $binding, $sql, 1);
+            }
+
+            call_user_func($callback, $sql, $event->time);
+        });
+    }
+}
+
+if ( ! function_exists('query_log')) {
+
+    /**
+     * @param Connection|string|null $connection
+     *
+     * @return void
+     */
+    function query_log($connection = null): void
+    {
+        query_listen(function ($query, $time): void {
+
+            $log = Container::getInstance()->make('log');
+
+            $log->debug('SQL query', compact('query', 'time'));
+
+        }, $connection);
+    }
+}
